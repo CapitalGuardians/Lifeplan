fe6f31c0a1e202117814faddd2debac48ec95cf0
Ziwei Xu <zix1@student.unimelb.edu.au>
2020-02-08 16:27:51 +1100

    Change datepicker hover colour

--- frontend/src/budget/CustomCalendar.js
+++ frontend/src/budget/CustomCalendar.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
-import _ from "lodash";
 
 export default function CustomCalendar(props) {
   const { newEvents } = props;
